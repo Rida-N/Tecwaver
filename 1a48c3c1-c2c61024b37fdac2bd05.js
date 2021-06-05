f48ccb2331d216450da4546c4f20eb740ce619bd
Rida <nowthursday@gmail.com>
2021-06-05 21:02:14 +0800

    Updates

---
+++ 1a48c3c1-c2c61024b37fdac2bd05.js
@@ -0,0 +1,2 @@
+(self.webpackChunktecwaver=self.webpackChunktecwaver||[]).push([[874],{7244:function(t,c,e){"use strict";e.d(c,{Goc:function(){return n}});var a=e(1046);function n(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"}}]})(t)}}}]);
+//# sourceMappingURL=1a48c3c1-c2c61024b37fdac2bd05.js.map
